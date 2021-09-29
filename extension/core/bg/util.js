1fe8c2aff489c8bf779fc249322aeaadcd78f663
Gildas <gildas.lormeau@gmail.com>
2021-09-29 02:34:31 +0200

    add fallback

--- extension/core/bg/util.js
+++ extension/core/bg/util.js
@@ -39,7 +39,11 @@ async function getMessages() {
 		} catch (error) {
 			if (language.includes("_")) {
 				language = language.split("_")[0];
-				response = await fetch(`/_locales/${language}/messages.json`);
+				try {
+					response = await fetch(`/_locales/${language}/messages.json`);
+				} catch (error) {
+					response = await fetch("/_locales/en/messages.json");
+				}
 			} else {
 				throw error;
 			}
