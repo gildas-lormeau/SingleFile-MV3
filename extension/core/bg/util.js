c29dd6c321c7366d2cf22c4844ec38c1a4021af9
Gildas <gildas.lormeau@gmail.com>
2021-09-29 02:31:44 +0200

    cache response

--- extension/core/bg/util.js
+++ extension/core/bg/util.js
@@ -23,22 +23,28 @@
 
 /* global browser, fetch */
 
+let messages;
+getMessages();
+
 export {
 	getMessages
 };
 
 async function getMessages() {
-	let language = (((await browser.i18n.getAcceptLanguages())[0]) || "en").replace(/-/, "_");
-	let response;
-	try {
-		response = await fetch(`/_locales/${language}/messages.json`);
-	} catch (error) {
-		if (language.includes("_")) {
-			language = language.split("_")[0];
+	if (!messages) {
+		let language = (((await browser.i18n.getAcceptLanguages())[0]) || "en").replace(/-/, "_");
+		let response;
+		try {
 			response = await fetch(`/_locales/${language}/messages.json`);
-		} else {
-			throw error;
+		} catch (error) {
+			if (language.includes("_")) {
+				language = language.split("_")[0];
+				response = await fetch(`/_locales/${language}/messages.json`);
+			} else {
+				throw error;
+			}
 		}
+		messages = await response.json();
 	}
-	return await response.json();
+	return messages;
 }
