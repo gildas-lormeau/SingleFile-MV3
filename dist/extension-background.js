91d5b367a64ff100ad8d3d8f9a01ffaffe55e856
Gildas <gildas.lormeau@gmail.com>
2021-09-29 02:34:50 +0200

    update built files

--- dist/extension-background.js
+++ dist/extension-background.js
@@ -1166,7 +1166,11 @@
 			} catch (error) {
 				if (language.includes("_")) {
 					language = language.split("_")[0];
-					response = await fetch(`/_locales/${language}/messages.json`);
+					try {
+						response = await fetch(`/_locales/${language}/messages.json`);
+					} catch (error) {
+						response = await fetch("/_locales/en/messages.json");
+					}
 				} else {
 					throw error;
 				}
