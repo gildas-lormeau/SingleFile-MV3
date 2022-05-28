bcc80a1b485c2ad8cc38c6cc3c59816fac6b12fb
Gildas <gildas.lormeau@gmail.com>
2022-05-28 02:50:32 +0200

    update CSSTree to version 2.1

--- src/single-file/modules/css-fonts-alt-minifier.js
+++ src/single-file/modules/css-fonts-alt-minifier.js
@@ -78,8 +78,8 @@ async function process(doc, stylesheets, fontURLs, fontTests) {
 	stylesheets.forEach(stylesheetInfo => {
 		const cssRules = stylesheetInfo.stylesheet.children;
 		if (cssRules) {
-			stats.rules.processed += cssRules.getSize();
-			stats.rules.discarded += cssRules.getSize();
+			stats.rules.processed += cssRules.size;
+			stats.rules.discarded += cssRules.size;
 			if (stylesheetInfo.mediaText && stylesheetInfo.mediaText != MEDIA_ALL) {
 				const mediaFontsDetails = createFontsDetailsInfo();
 				fontsDetails.medias.set("media-" + sheetIndex + "-" + stylesheetInfo.mediaText, mediaFontsDetails);
@@ -100,7 +100,7 @@ async function process(doc, stylesheets, fontURLs, fontTests) {
 			} else {
 				await processFontFaceRules(cssRules, sheetIndex, fontsDetails, fontURLs, fontTests, stats);
 			}
-			stats.rules.discarded -= cssRules.getSize();
+			stats.rules.discarded -= cssRules.size;
 		}
 	}));
 	return stats;
