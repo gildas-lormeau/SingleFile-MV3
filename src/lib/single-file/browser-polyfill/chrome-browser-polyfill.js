9f315d4fcac88e1fe09112e7c2ff4aabb78071f4
Gildas <gildas.lormeau@gmail.com>
2022-10-28 23:23:51 +0200

    use `runtime.sendNativeMessage`

--- src/lib/single-file/browser-polyfill/chrome-browser-polyfill.js
+++ src/lib/single-file/browser-polyfill/chrome-browser-polyfill.js
@@ -140,7 +140,15 @@ if (typeof globalThis == "undefined") {
 				remove: permissions => nativeAPI.permissions.remove(permissions)
 			},
 			runtime: {
-				connectNative: application => nativeAPI.runtime.connectNative(application),
+				sendNativeMessage: (application, message) => new Promise((resolve, reject) => {
+					nativeAPI.runtime.sendNativeMessage(application, message, result => {
+						if (nativeAPI.runtime.lastError) {
+							reject(nativeAPI.runtime.lastError);
+						} else {
+							resolve(result);
+						}
+					});
+				}),
 				getManifest: () => nativeAPI.runtime.getManifest(),
 				onMessage: {
 					addListener: listener => nativeAPI.runtime.onMessage.addListener((message, sender, sendResponse) => {
