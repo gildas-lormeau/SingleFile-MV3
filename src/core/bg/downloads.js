5f835542bda075d07558dbe564e57b51772d5b41
Gildas <gildas.lormeau@gmail.com>
2024-07-11 00:38:09 +0200

    move "ping" code into `index.js`

--- src/core/bg/downloads.js
+++ src/core/bg/downloads.js
@@ -117,9 +117,6 @@ async function onMessage(message, sender) {
 		business.saveUrls(message.urls);
 		return {};
 	}
-	if (message.method.endsWith(".ping")) {
-		return {};
-	}
 }
 
 async function downloadTabPage(message, tab) {
