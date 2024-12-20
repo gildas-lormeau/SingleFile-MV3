8b3c920dc89c309c8fee3a3a24e0c021dfc87b92
Gildas <gildas.lormeau@gmail.com>
2024-12-20 22:48:22 +0100

    fix script for edge

--- build-extension.sh
+++ build-extension.sh
@@ -24,7 +24,10 @@ rm singlefile-extension-chromium.zip singlefile-extension-edge.zip
 zip -r singlefile-extension-chromium.zip manifest.json lib _locales src
 
 cp src/core/bg/config.js config.copy.js
+cp manifest.json manifest.copy.json
+jq 'del(.oauth2)' manifest.json > manifest.tmp.json && mv manifest.tmp.json manifest.json
 sed -i "" 's/forceWebAuthFlow: false/forceWebAuthFlow: true/g' src/core/bg/config.js
 sed -i "" 's/image\/avif,//g' src/core/bg/config.js
 zip -r singlefile-extension-edge.zip manifest.json lib _locales src
 mv config.copy.js src/core/bg/config.js
+mv manifest.copy.json manifest.json
